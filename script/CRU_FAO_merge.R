1cadb18d1a85303ebbdf48f1386140af21b262d2
DESKTOP-I92DRIR\Eris- <ueki15937@gamil.com>
2018-06-05 11:27:59 +0900

    180605午前の進捗、これでデータ整形は終了？

--- script/CRU_FAO_merge.R
+++ script/CRU_FAO_merge.R
@@ -1,5 +1,4 @@
 require(countrycode)
-require(mgcv)
 
 order2<-"Maize"
 
@@ -31,10 +30,26 @@ CD <- merge(tmp, pre, by.x = c("year", "COWN"), by.y = c("year", "COWN"))
 names(CD) <- c("year", "connum", "tmp", "pre")
 CD <- cbind(CD, countrylis.csv[CD[,"connum"],c("cown","faon","country.name")])
 
+iso3c <- countrycode(CD$country.name, "country.name", "iso3c")
+CD <- cbind(CD, iso3c)
+rm(iso3c)
+
 faodata <- reshape(faodata, timevar = "Element", idvar = names(faodata)[!names(faodata)%in%c("Element","Value")],
                     direction = "wide")
-faodata <- subset(faodata, faodata$Area.Code <5000) #we do not need world sum
+faodata <- subset(faodata, as.numeric(faodata$Area.Code) <5000) #we do not need world sum
 
-ClimateFAO <- merge(CD, faodata, by.x = c("year", "faon"), by.y = c("Year","Area.Code"), all=T)
+#descriminate if area code ofFAO data is "iso3c" or "fao's original area code"
+#if 90% of area code is 3 letters, code is recognized as "iso3c"
+nchar.table <- table(nchar(as.character(faodata$Area.Code)))
+fao_code_type <- nchar.table["3"] > 0.9
+faocode <- c("faon", "iso3c")[1 + fao_code_type]
 
-CRUFAO <- na.omit(CFAO)
+#if iso3c is adopted, Russian federation is not sinchronized with USSR
+#this will be solved by individual input?
+
+ClimateFAO <- merge(CD, faodata, by.x = c("year", faocode), by.y = c("Year","Area.Code"), all=T)
+CRUFAO <- na.omit(ClimateFAO)
+
+#export merged data
+write.csv(ClimateFAO, "../produced_data/csv/merged_CRU_FAO_full.csv")
+write.csv(CRUFAO, "../produced_data/csv/merged_CRU_FAO.csv")
